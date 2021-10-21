0c64b1f1186bdd62e5797e957337709be9e86ac8
serubi <serubi@live.com>
2021-10-21 11:55:44 +0200

    Accept payload and send response

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
 
 namespace Assignment3
 {
@@ -14,9 +16,47 @@ namespace Assignment3
 
             while(true)
             {
-                server.AcceptTcpClient();
+                var client = server.AcceptTcpClient();
                 Console.WriteLine("Client connected...");
+
+                var stream = client.GetStream();
+
+                var buffer = new byte[2048];
+
+                var rdCnt = stream.Read(buffer);
+
+                var json = Encoding.UTF8.GetString(buffer, 0, rdCnt);
+
+                Console.WriteLine("Client says: " + json);
+                //Console.WriteLine($"Read count was {rdCnt}");
+
+                var payload = JsonSerializer.Deserialize<Payload>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }); // Second parameter ensures that it is case insensitive
+                
+                var response = new Response("Missing method", "Test");
+                var jsonResponse = JsonSerializer.Serialize(response);
+                var responseBytes = Encoding.UTF8.GetBytes(jsonResponse);
+                stream.Write(responseBytes);
             }
+        }
+    }
+
+    class Payload
+    {
+        public string Method { get; set; }
+        public string Path { get; set; }
+        public string Date { get; set; }
+        public string Body { get; set; }
+    }
+
+    class Response
+    {
+        public string Status { get; set; }
+        public string Body { get; set; }
+
+        public Response(string StatusText, string BodyText)
+        {
+            Status = StatusText;
+            Body = BodyText;
         }
     }
 }
