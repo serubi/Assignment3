7dd6350f473baac653e9d1298a4719bc332bb41f
serubi <serubi@live.com>
2021-10-21 22:36:39 +0200

    Validate methods, paths, dates (somewhat)

--- Program.cs
+++ Program.cs
@@ -8,8 +8,11 @@ namespace Assignment3
 {
     class Program
     {
+        delegate void Transformer(string x);
+
         static void Main(string[] args)
         {
+            //var validMethods = ["create", "read", "update", "delete", "echo"];
             var server = new TcpListener(IPAddress.Loopback, 5000);
             server.Start();
             Console.WriteLine("Server has started...");
@@ -31,20 +34,63 @@ namespace Assignment3
                     var payload = JsonSerializer.Deserialize<Payload>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }); // Second parameter ensures that it is case insensitive
 
                     var response = new Response();
+                    response.Status = "";
 
+                    // Check payload for valid Method
                     if (payload.Method == null)
                     {
                         Console.WriteLine("Missing method");
-                        response.Status = "Missing method";
+                        response.Status += (response.Status.Length == 0 ? "4 " : ", ") + "Missing method";
+                    } else
+                    {
+                        var protocolMethod = new Transformer(Create);
+
+                        switch (payload.Method.ToLower())
+                        {
+                            case "create":
+                                protocolMethod = new Transformer(Create);
+                                break;
+                            default:
+                                Console.WriteLine("Illegal method");
+                                response.Status += (response.Status.Length == 0 ? "4 " : ", ") + "Illegal method";
+                                break;
+                        }
                     }
 
-                    if (payload.Method == "xxxx")
+                    // Check payload for valid Path
+                    if (payload.Path == null)
                     {
-                        Console.WriteLine("Illegal method");
-                        response.Status = "Illegal method";
+                        Console.WriteLine("Missing resource");
+                        response.Status += (response.Status.Length == 0 ? "4 " : ", ") + "Missing resource";
+                    }
+                    else
+                    {
+                        switch (payload.Path.ToLower())
+                        {
+                            case "test":
+
+                                break;
+                            default:
+                                Console.WriteLine("Illegal resource");
+                                response.Status += (response.Status.Length == 0 ? "4 " : ", ") + "Illegal resource";
+                                break;
+                        }
+                    }
+
+                    // Check payload for valid Date
+                    if (payload.Date == null)
+                    {
+                        Console.WriteLine("Missing date");
+                        response.Status += (response.Status.Length == 0 ? "4 " : ", ") + "Missing date";
+                    }
+                    else if (!int.TryParse(payload.Date, out int timestamp))
+                    {
+                        Console.WriteLine("Illegal date");
+                        response.Status += (response.Status.Length == 0 ? "4 " : ", ") + "Illegal date";
                     }
 
                     var jsonResponse = JsonSerializer.Serialize(response);
+                    Console.WriteLine(jsonResponse);
                     var responseBytes = Encoding.UTF8.GetBytes(jsonResponse);
                     stream.Write(responseBytes);
                 }
@@ -53,6 +99,11 @@ namespace Assignment3
                     Console.WriteLine("SocketException: {0}", e);
                 }
             }
+        }
+
+        static void Create(string Path)
+        {
+            Console.WriteLine("Creating item");
         }
     }
 
