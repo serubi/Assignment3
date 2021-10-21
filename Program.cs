2a94e1458edbab84343acdfeaa7d418d06712b24
serubi <serubi@live.com>
2021-10-21 09:10:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new TcpListener(IPAddress.Loopback, 5000);
+            server.Start();
+            Console.WriteLine("Server has started...");
+
+
+        }
+    }
+}
